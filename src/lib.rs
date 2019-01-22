aaf70181ab674612845c57aec6ee7e3803a74de0
sharkdp <davidpeter@web.de>
2019-01-22 19:25:29 +0100

    Use Iterator instead of Vec

--- src/lib.rs
+++ src/lib.rs
@@ -135,6 +135,47 @@ impl Indicator {
 
 type FileNameSuffix = String;
 
+/// Iterator over the path components with their respective style.
+pub struct StyledComponents<'a> {
+    /// Reference to the underlying LsColors object
+    lscolors: &'a LsColors,
+
+    /// Full path to the current component
+    component_path: PathBuf,
+
+    /// Underlying iterator over the path components
+    components: std::iter::Peekable<std::path::Components<'a>>,
+}
+
+impl<'a> Iterator for StyledComponents<'a> {
+    type Item = (OsString, Option<&'a Style>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(component) = self.components.next() {
+            let mut component_str = component.as_os_str().to_os_string();
+
+            self.component_path.push(&component_str);
+            let style = self.lscolors.style_for_path(&self.component_path);
+
+            if self.components.peek().is_some() {
+                match component {
+                    // Prefix needs no separator, as it is always followed by RootDir.
+                    // RootDir is already a separator.
+                    Component::Prefix(_) | Component::RootDir => {}
+                    // Everything else uses a separator that is painted the same way as the component.
+                    Component::CurDir | Component::ParentDir | Component::Normal(_) => {
+                        component_str.push(MAIN_SEPARATOR.to_string());
+                    }
+                }
+            }
+
+            Some((component_str, style))
+        } else {
+            None
+        }
+    }
+}
+
 const LS_COLORS_DEFAULT: &str = "rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:";
 
 /// Holds information about how different file system entries should be colorized / styled.
@@ -268,43 +309,16 @@ impl LsColors {
         None
     }
 
-    /// Get ANSI styles for each component of a given path. Components already
-    /// include the path separator symbol, if required. For a path like
-    /// `foo/bar/test.md`, this would return three pairs for the components
-    /// `foo/`, `bar/` and `test.md` together with their respective styles.
-    pub fn style_for_path_components<P: AsRef<Path>>(
-        &self,
-        path: P,
-    ) -> Vec<(OsString, Option<&Style>)> {
-        let mut styled_components = vec![];
-
-        // Full path to the current component.
-        let mut component_path = PathBuf::new();
-
-        // Traverse the path and colorize each component
-        let mut components = path.as_ref().components().peekable();
-        while let Some(component) = components.next() {
-            let mut component_str = component.as_os_str().to_os_string();
-
-            component_path.push(&component_str);
-            let style = self.style_for_path(&component_path);
-
-            if components.peek().is_some() {
-                match component {
-                    // Prefix needs no separator, as it is always followed by RootDir.
-                    // RootDir is already a separator.
-                    Component::Prefix(_) | Component::RootDir => {}
-                    // Everything else uses a separator that is painted the same way as the component.
-                    Component::CurDir | Component::ParentDir | Component::Normal(_) => {
-                        component_str.push(MAIN_SEPARATOR.to_string());
-                    }
-                }
-            }
-
-            styled_components.push((component_str, style));
+    /// Get ANSI styles for each component of a given path. Components already include the path
+    /// separator symbol, if required. For a path like `foo/bar/test.md`, this would return an
+    /// iterator over three pairs for the three path components `foo/`, `bar/` and `test.md`
+    /// together with their respective styles.
+    pub fn style_for_path_components<'a>(&'a self, path: &'a Path) -> StyledComponents<'a> {
+        StyledComponents {
+            lscolors: &self,
+            component_path: PathBuf::new(),
+            components: path.components().peekable(),
         }
-
-        styled_components
     }
 
     /// Get the ANSI style for a certain `Indicator` (regular file, directory, symlink, ...). Note
@@ -494,7 +508,9 @@ mod tests {
 
         let lscolors = LsColors::from_string("di=34:ln=35:*.png=36");
 
-        let mut components = lscolors.style_for_path_components(path_via_symlink);
+        let mut components: Vec<_> = lscolors
+            .style_for_path_components(&path_via_symlink)
+            .collect();
 
         let (c_file, style_file) = components.pop().unwrap();
         assert_eq!("test-file.png", c_file);
