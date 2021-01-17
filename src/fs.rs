bc95124f23d2f1c14616e6a97f5ae9e6ab786697
Julius Michaelis <gitter@liftm.de>
2021-01-18 00:26:33 +0900

    Fix compiling for wasi

--- src/fs.rs
+++ src/fs.rs
@@ -1,14 +1,12 @@
 use std::fs;
 
 #[cfg(any(unix, target_os = "redox"))]
-use std::os::unix::fs::PermissionsExt;
-
-#[cfg(any(unix, target_os = "redox"))]
 pub fn is_executable(md: &fs::Metadata) -> bool {
+    use std::os::unix::fs::PermissionsExt;
     md.permissions().mode() & 0o111 != 0
 }
 
-#[cfg(windows)]
+#[cfg(any(windows, target_os = "wasi"))]
 pub fn is_executable(_: &fs::Metadata) -> bool {
     false
 }
