dde45bfa6ca8a8540df6c431235287083e49a1b7
Tavian Barnes <tavianator@tavianator.com>
2021-09-16 12:29:03 -0400

    Add support for more indicators
    Partially addresses #6.

--- src/fs.rs
+++ src/fs.rs
@@ -1,12 +1,24 @@
 use std::fs;
 
 #[cfg(any(unix, target_os = "redox"))]
-pub fn is_executable(md: &fs::Metadata) -> bool {
-    use std::os::unix::fs::PermissionsExt;
-    md.permissions().mode() & 0o111 != 0
+use std::os::unix::fs::MetadataExt;
+
+/// Get the UNIX-style mode bits from some metadata if available, otherwise 0.
+#[allow(unused_variables)]
+pub fn mode(md: &fs::Metadata) -> u32 {
+    #[cfg(any(unix, target_os = "redox"))]
+    return md.mode();
+
+    #[cfg(not(any(unix, target_os = "redox")))]
+    return 0;
 }
 
-#[cfg(any(windows, target_os = "wasi"))]
-pub fn is_executable(_: &fs::Metadata) -> bool {
-    false
+/// Get the number of hard links to a file, or 1 if unknown.
+#[allow(unused_variables)]
+pub fn nlink(md: &fs::Metadata) -> u64 {
+    #[cfg(any(unix, target_os = "redox"))]
+    return md.nlink();
+
+    #[cfg(not(any(unix, target_os = "redox")))]
+    return 1;
 }
